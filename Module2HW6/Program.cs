3cfd4d8ba015e42d85dc45d4e96d0620bbdd97bc
AngrySonya <Photographersofi@gmail.com>
2021-07-23 10:34:19 +0300

    add basic logic

---
+++ Module2HW6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module2HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
